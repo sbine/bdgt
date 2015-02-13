cef76c5858fa6a193ea60f251c12f2814dcdc789
Sara Bine <sara@sarabine.com>
2015-02-13 14:37:55 -0700

    Add missing menu items

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -39,6 +39,8 @@
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
 					<li><a href="/">Home</a></li>
+					<li><a href="/accounts">Accounts</a></li>
+					<li><a href="/transactions">Transactions</a></li>
 				</ul>
 
 				<ul class="nav navbar-nav navbar-right">
