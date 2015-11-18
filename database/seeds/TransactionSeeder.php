cfa694b18b786b713008f9d635b9f530029187b4
Sara Bine <sara@sarabine.com>
2015-11-17 23:50:20 -0700

    Ensure at least one transaction is created for the dummy user

--- database/seeds/TransactionSeeder.php
+++ database/seeds/TransactionSeeder.php
@@ -14,6 +14,21 @@ class TransactionSeeder extends Seeder
     {
         $faker = Faker\Factory::create();
 
+        // Ensure at least one transaction is created for the dummy user
+        Transaction::create([
+                'id' => null,
+                'user_id' => 1,
+                'date' => $faker->dateTimeBetween('-2 years'),
+                'account_id' => $faker->numberBetween(1, 3),
+                'category_id' => $faker->numberBetween(1, 3),
+                'bill_id' => $faker->numberBetween(1, 7),
+                'payee' => $faker->company(),
+                'amount' => $faker->randomFloat(2, 0, 200),
+                'inflow' => $faker->boolean(),
+                'cleared' => $faker->boolean(),
+                'flair' => 'lightgray'
+            ]);
+
         for ($i = 0; $i < 30; $i++) {
             Transaction::create([
                 'id' => null,
