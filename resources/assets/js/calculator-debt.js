3c54cab6466af0e710d86b1ab8669d2f96f72423
Ryan O'Hara <ryan@ryanohara.co>
2016-05-22 22:33:18 -0600

    Functional debt calculator on the frontend

--- resources/assets/js/calculator-debt.js
+++ resources/assets/js/calculator-debt.js
@@ -1,9 +1,54 @@
-var Loan = function(amount, interestPercent, minimumPayment) {
+var Loan = function(amount, interestRate, minimumPayment) {
   this.amount = amount;
-  this.interestPercent = interestPercent;
+  this.interestRate = interestRate;
   this.minimumPayment = minimumPayment;
+  this.payment = minimumPayment;
 };
 
-Loan.prototype.interest = function(amount) {
-	return ((this.interestPercent / 100) * amount);
+Loan.prototype.makePayment = function(amount) {
+	this.payment = amount;
+
+	return this.calculate();
 };
+
+Loan.prototype.getPrincipal = function() {
+	if (this.amount === undefined || this.amount === NaN) {
+		return 0;
+	}
+
+	return this.amount;
+}
+
+Loan.prototype.calculate = function() {
+	var i = 0;
+
+	var principal = this.getPrincipal();
+
+	var interestPaid    = 0;
+	var date            = moment();
+	var currentBalance  = principal;
+
+	while (currentBalance > 0) {
+		date.add(1, 'M');
+
+		currentBalance      -= this.payment;
+		currentBalance      *= 1 + this.interestRate;
+		additionalInterest  = (currentBalance * this.interestRate);
+		interestPaid        += additionalInterest;
+
+		if (i > 960 || currentBalance >= Infinity) {
+			return { interest: -1 }
+		}
+	}
+
+	return {
+		date:       date,
+		interest:   interestPaid
+	};
+}
+
+Loan.prototype.formatWithCommas = function(num) {
+	// This function will add commas and
+	// round a float to the hundredth place.
+	return num.toString().replace(/(\.\d{2})\d+$/, "$1").replace(/(?=(\d{3})+(?!\d))/g, ",");
+}
