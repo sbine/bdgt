e21dec3193fa0c498da1f62f6ea06c8086e4632e
Sara Bine <sara@sarabine.com>
2016-08-23 18:28:28 -0600

    DRY repository binding

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -23,65 +23,28 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(
-            'Bdgt\Repositories\Contracts\AccountRepositoryInterface',
-            function ($app, $parameters) {
-                $model = reset($parameters);
-                if (!$model) {
-                    $model = $app->make('Bdgt\Resources\Account');
-                }
-                $repository = new \Bdgt\Repositories\Eloquent\EloquentAccountRepository($model);
-                return $this->scopeForCurrentTenant($repository);
-            }
-        );
+        $resources = [
+            'Account',
+            'Bill',
+            'Category',
+            'Goal',
+            'Transaction',
+        ];
 
-        $this->app->bind(
-            'Bdgt\Repositories\Contracts\BillRepositoryInterface',
-            function ($app, $parameters) {
-                $model = reset($parameters);
-                if (!$model) {
-                    $model = $app->make('Bdgt\Resources\Bill');
+        foreach ($resources as $resource) {
+            $this->app->bind(
+                'Bdgt\Repositories\Contracts\\' . $resource . 'RepositoryInterface',
+                function ($app, $parameters) use ($resource) {
+                    $model = reset($parameters);
+                    if (!$model) {
+                        $model = $app->make('Bdgt\Resources\\' . $resource);
+                    }
+                    $repositoryPath = '\Bdgt\Repositories\Eloquent\Eloquent' . $resource . 'Repository';
+                    $repository = new $repositoryPath($model);
+                    return $this->scopeForCurrentTenant($repository);
                 }
-                $repository = new \Bdgt\Repositories\Eloquent\EloquentBillRepository($model);
-                return $this->scopeForCurrentTenant($repository);
-            }
-        );
-
-        $this->app->bind(
-            'Bdgt\Repositories\Contracts\CategoryRepositoryInterface',
-            function ($app, $parameters) {
-                $model = reset($parameters);
-                if (!$model) {
-                    $model = $app->make('Bdgt\Resources\Category');
-                }
-                $repository = new \Bdgt\Repositories\Eloquent\EloquentCategoryRepository($model);
-                return $this->scopeForCurrentTenant($repository);
-            }
-        );
-
-        $this->app->bind(
-            'Bdgt\Repositories\Contracts\GoalRepositoryInterface',
-            function ($app, $parameters) {
-                $model = reset($parameters);
-                if (!$model) {
-                    $model = $app->make('Bdgt\Resources\Goal');
-                }
-                $repository = new \Bdgt\Repositories\Eloquent\EloquentGoalRepository($model);
-                return $this->scopeForCurrentTenant($repository);
-            }
-        );
-
-        $this->app->bind(
-            'Bdgt\Repositories\Contracts\TransactionRepositoryInterface',
-            function ($app, $parameters) {
-                $model = reset($parameters);
-                if (!$model) {
-                    $model = $app->make('Bdgt\Resources\Transaction');
-                }
-                $repository = new \Bdgt\Repositories\Eloquent\EloquentTransactionRepository($model);
-                return $this->scopeForCurrentTenant($repository);
-            }
-        );
+            );
+        }
     }
 
     private function scopeForCurrentTenant($repository)
