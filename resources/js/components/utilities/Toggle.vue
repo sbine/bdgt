ffc978aac6729803cd953fda9307854acd681410
Sara Bine <sara@sarabine.com>
2019-04-20 11:09:54 -0700

    Add Toggle component and use for nav dropdowns

---
+++ resources/js/components/utilities/Toggle.vue
@@ -0,0 +1,41 @@
+<template>
+    <div v-on-clickaway="blur">
+        <slot
+            :is-on="isOn"
+            :set-to="setTo"
+            :toggle="toggle"
+        />
+    </div>
+</template>
+
+<script>
+import { mixin as clickaway } from 'vue-clickaway'
+
+export default {
+    mixins: [ clickaway ],
+
+    props: {
+        closeOnBlur: { type: Boolean },
+    },
+
+    data() {
+        return {
+            isOn: false,
+        }
+    },
+
+    methods: {
+        setTo(value) {
+            this.isOn = !! value
+        },
+        toggle() {
+            this.isOn = ! this.isOn
+        },
+        blur() {
+            if (this.closeOnBlur) {
+                this.setTo(false)
+            }
+        }
+    }
+}
+</script>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -7,27 +7,61 @@
 					<span>bdgt</span>
 				</a>
 			</div>
-			@auth
-				<div class="{{ (request()->route()->getName() == 'dashboard' ? 'active' : '') }}">
-					<a class="block text-gray-400 hover:text-white px-6 py-4" href="{{ route('dashboard') }}">Dashboard</a>
-				</div>
-				<div class="{{ (strpos(request()->route()->getName(), 'goals') !== false ? 'active' : '') }}">
-					<a class="block text-gray-400 hover:text-white px-6 py-4" href="{{ route('goals.index') }}">{{ trans('labels.goals.plural') }}</a>
-				</div>
-				<div class="{{ (strpos(request()->route()->getName(), 'bills') !== false ? 'active' : '') }}">
-					<a class="block text-gray-400 hover:text-white px-6 py-4" href="{{ route('bills.index') }}">{{ trans('labels.bills.plural') }}</a>
-				</div>
-				<div {{ (strpos(request()->route()->getName(), 'reports.spending_by_category') !== false ? 'active' : '') }}>
-					<a class="block text-gray-400 hover:text-white px-6 py-4" href="/reports/categorial">{{ trans('labels.reports.spending_by_category') }}</a>
-				</div>
-				<div {{ (strpos(request()->route()->getName(), 'reports.spending_over_time') !== false ? 'active' : '') }}>
-					<a class="block text-gray-400 hover:text-white px-6 py-4" href="/reports/spending">{{ trans('labels.reports.spending_over_time') }}</a>
-				</div>
-			@endauth
 
-			<div {{ (strpos(request()->route()->getName(), 'calculators.debt') !== false ? 'active' : '') }}>
-				<a class="block text-gray-400 hover:text-white px-6 py-4" href="{{ route('calculators.debt') }}">Debt Calculator</a>
-			</div>
+			<template v-cloak>
+				@auth
+					<div class="{{ (request()->route()->getName() == 'dashboard' ? 'active' : '') }}">
+						<a class="block text-gray-400 hover:text-white px-6 py-4" href="{{ route('dashboard') }}">Dashboard</a>
+					</div>
+					<div class="{{ (strpos(request()->route()->getName(), 'accounts') !== false ? 'active' : '') }}">
+						<a class="block text-gray-400 hover:text-white px-6 py-4" href="{{ route('accounts.index') }}">{{ trans('labels.accounts.plural') }}</a>
+					</div>
+					<div class="{{ (strpos(request()->route()->getName(), 'categories') !== false ? 'active' : '') }}">
+						<a class="block text-gray-400 hover:text-white px-6 py-4" href="{{ route('categories.index') }}">{{ trans('labels.categories.plural') }}</a>
+					</div>
+					<div class="{{ (strpos(request()->route()->getName(), 'goals') !== false ? 'active' : '') }}">
+						<a class="block text-gray-400 hover:text-white px-6 py-4" href="{{ route('goals.index') }}">{{ trans('labels.goals.plural') }}</a>
+					</div>
+					<div class="{{ (strpos(request()->route()->getName(), 'bills') !== false ? 'active' : '') }}">
+						<a class="block text-gray-400 hover:text-white px-6 py-4" href="{{ route('bills.index') }}">{{ trans('labels.bills.plural') }}</a>
+					</div>
+
+					<toggle close-on-blur>
+						<div slot-scope="{ isOn, toggle }">
+							<a class="block text-gray-400 hover:text-white px-6 py-4" href="#" @click="toggle">
+								Reports
+								<font-awesome-icon v-if="isOn" icon="caret-up" class="ml-2"></font-awesome-icon>
+								<font-awesome-icon v-else icon="caret-down" class="ml-2"></font-awesome-icon>
+							</a>
+
+							<div v-if="isOn" class="absolute bg-white shadow-md rounded mt-1">
+								<div {{ (strpos(request()->route()->getName(), 'reports.spending_by_category') !== false ? 'active' : '') }}>
+									<a class="block text-gray-800 hover:text-gray-600 px-6 py-4" href="/reports/categorial">{{ trans('labels.reports.spending_by_category') }}</a>
+								</div>
+								<div {{ (strpos(request()->route()->getName(), 'reports.spending_over_time') !== false ? 'active' : '') }}>
+									<a class="block text-gray-800 hover:text-gray-600 px-6 py-4" href="/reports/spending">{{ trans('labels.reports.spending_over_time') }}</a>
+								</div>
+							</div>
+						</div>
+					</toggle>
+				@endauth
+
+				<toggle close-on-blur>
+					<div slot-scope="{ isOn, toggle }">
+						<a class="block text-gray-400 hover:text-white px-6 py-4" href="#" @click="toggle">
+							Calculators
+							<font-awesome-icon v-if="isOn" icon="caret-up" class="ml-2"></font-awesome-icon>
+							<font-awesome-icon v-else icon="caret-down" class="ml-2"></font-awesome-icon>
+						</a>
+
+						<div v-if="isOn" class="absolute bg-white shadow-md rounded mt-1">
+							<div {{ (strpos(request()->route()->getName(), 'calculators.debt') !== false ? 'active' : '') }}>
+								<a class="block text-gray-800 hover:text-gray-600 px-6 py-4" href="{{ route('calculators.debt') }}">Debt Calculator</a>
+							</div>
+						</div>
+					</div>
+				</toggle>
+			</template>
 		</div>
 
 		<div class="flex items-center justify-center">
