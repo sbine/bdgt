ab367de90a39cb208e945a48e65d5e3127bfe6d4
Sara Bine <sara@sarabine.com>
2019-11-01 21:56:02 -0700

    Update dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,17 +2,6 @@ const mix = require('laravel-mix')
 const tailwindcss = require('tailwindcss')
 const webpack = require('webpack')
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.webpackConfig({
    plugins: [
       new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
