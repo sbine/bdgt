136ccb906412dd745a26b02eb0ecc4593b96eeb7
Sara Bine <sara@sarabine.com>
2020-02-06 19:58:02 -0800

    Add Telescope in local envs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        if ($this->app->isLocal()) {
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
