86cbfb301fb312af99dba822307fd6e1efa6b2f4
Adam Stachowicz <saibamenppl@gmail.com>
2020-10-04 16:28:01 +0200

    Update crud.php

--- resources/lang/pl/crud.php
+++ resources/lang/pl/crud.php
@@ -5,30 +5,30 @@ return [
         'created' => 'Pomyślnie utworzono konto!',
         'updated' => 'Pomyslnie zaktualizowano konto!',
         'deleted' => 'Pomyslnie usunięto konto.',
-        'error'   => 'Wystąpił błąd podczas zapisu konta. Proszę sprobować ponownie.'
+        'error' => 'Wystąpił błąd podczas zapisu konta. Proszę sprobować ponownie.',
     ],
     'bills' => [
         'created' => 'Pomyślnie dodano rachunek!',
         'updated' => 'Pomyślnie zaktualizowano rachunek!',
         'deleted' => 'Pomyślnie usunięto rachunek.',
-        'error'   => 'Wystąpił błąd podczas zapisu rachunku. Proszę sprobować ponownie.'
+        'error' => 'Wystąpił błąd podczas zapisu rachunku. Proszę sprobować ponownie.',
     ],
     'categories' => [
         'created' => 'Pomyślnie utworzono kategorię!',
         'updated' => 'Pomyślnie zaktualizowano kategorię!',
         'deleted' => 'Pomyślnie usunięto kategorię.',
-        'error'   => 'Wystąpił błąd podczas zapisu kategorii. Proszę sprobować ponownie.'
+        'error' => 'Wystąpił błąd podczas zapisu kategorii. Proszę sprobować ponownie.',
     ],
     'goals' => [
         'created' => 'Pomyślnie utworzono cel oszczędnościowy!',
         'updated' => 'Pomyślnie zaktualizowano cel oszczędnościowy!',
         'deleted' => 'Pomyślnie usunięto cel oszczędnościowy.',
-        'error'   => 'Wystąpił błąd podczas zapisu celu oszczędnościowego. Proszę sprobować ponownie.'
+        'error' => 'Wystąpił błąd podczas zapisu celu oszczędnościowego. Proszę sprobować ponownie.',
     ],
     'transactions' => [
         'created' => 'Pomyślnie dodano transakcję!',
         'updated' => 'Pomyślnie zaktualizowano transakcję!',
         'deleted' => 'Pomyślnie usunięto transakcję.',
-        'error'   => 'Wystąpił błąd podczas zapisu transakcji. Proszę sprobować ponownie.'
+        'error' => 'Wystąpił błąd podczas zapisu transakcji. Proszę sprobować ponownie.',
     ],
 ];
