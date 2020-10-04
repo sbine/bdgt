a5000adf4a95ea53086cda254df3eb21ef87dcdf
Adam Stachowicz <saibamenppl@gmail.com>
2020-10-04 16:50:58 +0200

    Update passwords.php

--- resources/lang/pl/passwords.php
+++ resources/lang/pl/passwords.php
@@ -13,9 +13,9 @@ return [
     |
     */
 
-    'password' => 'Hasło musi mieć przynajmniej sześć znaków i zgadzać się z potwierdzeniem.',
     'reset'    => 'Hasło zostało zresetowane!',
     'sent'     => 'Przypomnienie hasła zostało wysłane!',
+    'throttled' => 'Proszę zaczekać zanim spróbujesz ponownie.',
     'token'    => 'Token resetowania hasła jest nieprawidłowy.',
     'user'     => 'Nie znaleziono użytkownika z takim adresem e-mail.',
 
