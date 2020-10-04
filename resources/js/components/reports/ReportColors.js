129ede7f15b9afa682d1fccfc0201a3856b34f38
Cody Metz <ccmetz@bioneos.com>
2020-10-04 13:39:35 -0500

    Add new report colors, move colors to constant

---
+++ resources/js/components/reports/ReportColors.js
@@ -0,0 +1,16 @@
+/**
+* Colors for all reports
+*/
+const ReportColors = [
+    '#FFB067', // Orange
+    '#104210', // Forest green
+    '#FF7077', // Coral
+    '#ACEEF3', // Cyan
+    '#FF4500', // Red orange
+    '#FF8300', // Darker orange
+    '#DF362D', // Medium red
+    '#B7AC44', // Olive green
+    '#7A871E', // Darker olive green
+];
+
+export default ReportColors; 
--- resources/js/components/reports/SpendingByCategory.vue
+++ resources/js/components/reports/SpendingByCategory.vue
@@ -5,6 +5,7 @@
 <script>
 import Chart from 'chart.js'
 import dayjs from 'dayjs'
+import colors from './ReportColors.js'
 
 export default {
     props: {
@@ -16,17 +17,7 @@ export default {
             chart: null,
             datasets: [],
             labels: [],
-            colors: [
-                '#4D4D4D',
-                '#5DA5DA',
-                '#FAA43A',
-                '#60BD68',
-                '#F17CB0',
-                '#B2912F',
-                '#B276B2',
-                '#DECF3F',
-                '#F15854',
-            ],
+            colors: colors
         }
     },
 
