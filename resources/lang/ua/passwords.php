421a43d98f53ed5c6add94b3ba6c3c51e2724680
Petro-Ivvysoft <iqblackman@gmail.com>
2020-10-05 14:37:20 +0300

    Create passwords.php

---
+++ resources/lang/ua/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Ваш пароль був змінений!',
+    'sent' => 'Ми відправили посилання на зміну пароля на пошту!',
+    'throttled' => 'Будь ласка, подолжіте перед спробою знову.',
+    'token' => 'Цей токен зміни пароля невірний.',
+    'user' => "Ми не можемо знайти користувача з такою поштою.",
+
+];
