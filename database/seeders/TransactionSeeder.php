2493f81f7a430855b8fb486c213e5f24b0865a2b
Sara Bine <sara@sarabine.com>
2024-09-11 19:12:41 -0600

    Upgrade to Laravel 11

--- database/seeders/TransactionSeeder.php
+++ database/seeders/TransactionSeeder.php
@@ -11,22 +11,18 @@ class TransactionSeeder extends Seeder
     public function run()
     {
         User::with(['accounts', 'categories', 'bills'])->get()->each(function (User $user) {
-            Transaction::insert(
-                Transaction::factory()->count(30)->make([
-                    'user_id' => $user->id,
-                    'account_id' => function () use ($user) {
-                        return $user->accounts->random();
-                    },
-                    'category_id' => function () use ($user) {
-                        return $user->categories->random();
-                    },
-                    'bill_id' => function () use ($user) {
-                        return $user->bills->random();
-                    },
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ])->toArray()
-            );
+            Transaction::factory()->count(30)->create([
+                'user_id' => $user->id,
+                'account_id' => function () use ($user) {
+                    return $user->accounts->random();
+                },
+                'category_id' => function () use ($user) {
+                    return $user->categories->random();
+                },
+                'bill_id' => function () use ($user) {
+                    return $user->bills->random();
+                }
+            ]);
         });
     }
 }
